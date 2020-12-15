945f6bf7bc21852f8aa9d30f6c946e1f296e2412
SignatureJed <kong.3392.th@gmail.com>
2020-12-15 16:44:36 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,39 @@
 #include<iostream>
 using namespace std;
 
+
+
+
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+
+int integer_number;
+int Even_number=0;
+int Odd_number=0;
+
+    while (1)
+    {
+        cout << "Enter an integer: ";
+        cin >> integer_number;
+
+        if (integer_number==0)
+        {
+            cout << "#Even numbers = "<< Even_number<<endl;
+            cout << "#Odd numbers = " << Odd_number;
+            
+            break;
+        }
+        
+        if (integer_number%2==0)
+        {
+            Even_number = Even_number+1;
+        }else
+        {
+            Odd_number = Odd_number+1;
+        }
+        
+        
+        
+    }
     return 0;
 }
